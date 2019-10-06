701026346ce9f9d186949d98aeaed44eb0850fe2
Will Hoskings <will@localhost.localdomain>
2019-10-06 23:08:55 +0100

    Add testsf for JS api

--- test/test.js
+++ test/test.js
@@ -1,21 +1,27 @@
 var assert = require('assert')
 var getStdin = require('get-stdin')
 var got = require('got')
+var jsapi = require('../src/hastebin')
 
-getStdin()
+var testData = 'test'
 
-.then(function (stdin) {
-  var hastebinUrl = stdin.trim()
-  assert(hastebinUrl.match(/^https:\/\/hastebin.com\/.*/) != null, 'stout must be a hastebin url')
+function testOutput (input) {
+  var hastebinUrl = input.trim()
+  assert(hastebinUrl.match(/^https:\/\/hastebin.com\/.*/) != null, 'output must be a hastebin url')
   var key = hastebinUrl.split('/').slice(-1)[0]
   return got('https://hastebin.com/raw/' + key)
-})
 
-.then(function (result) {
-  assert.equal(result.body.trim(), 'test', 'hastebin contents must equal test data')
-})
+  .then(function (result) {
+    assert.equal(result.body.trim(), testData, 'hastebin contents must equal test data')
+  })
+}
 
-.catch(function (err) {
-  console.error(err)
-  process.exit(1)
+console.log('testing stdin...')
+getStdin().then(testOutput).then(function () {
+  console.log('testing jsapi non-raw...')
+  jsapi.createPaste(testData).then(testOutput)
+  .then(function () {
+    console.log('testing jsapi raw...')
+    jsapi.createPaste(testData, { raw: true }).then(testOutput)
+  })
 })
