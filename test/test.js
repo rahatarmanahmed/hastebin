ab7f3116677a74e4e004926f4afe76d5154b53d5
Rahat Ahmed <rahatarmanahmed@gmail.com>
2019-10-08 23:18:34 -0500

    Update to standard@11

--- test/test.js
+++ test/test.js
@@ -11,17 +11,17 @@ function assertHastebinUrlMatchesTestData (input) {
   var key = hastebinUrl.split('/').slice(-1)[0]
   return got('https://hastebin.com/raw/' + key)
 
-  .then(function (result) {
-    assert.equal(result.body.trim(), testData, 'hastebin contents must equal test data')
-  })
+    .then(function (result) {
+      assert.equal(result.body.trim(), testData, 'hastebin contents must equal test data')
+    })
 }
 
 console.log('testing stdin...')
 getStdin().then(assertHastebinUrlMatchesTestData).then(function () {
   console.log('testing jsapi non-raw...')
   jsapi.createPaste(testData).then(assertHastebinUrlMatchesTestData)
-  .then(function () {
-    console.log('testing jsapi raw...')
-    jsapi.createPaste(testData, { raw: true }).then(assertHastebinUrlMatchesTestData)
-  })
+    .then(function () {
+      console.log('testing jsapi raw...')
+      jsapi.createPaste(testData, { raw: true }).then(assertHastebinUrlMatchesTestData)
+    })
 })
